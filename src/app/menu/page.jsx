3da5830a95fb90d4e760e243fa4c9f862dc62223
DURIA SAFOUR <duriasafour@stu.topkapi.edu.tr>
2026-06-11 16:41:49 +0300

    Kodda guncellemeler yapildi

--- src/app/menu/page.jsx
+++ src/app/menu/page.jsx
@@ -5,17 +5,19 @@ import MenuWrapper from "../components/product/MenuWrapper";
 export const dynamic = "force-dynamic";
 
 async function getMenuData() {
- 
-  const baseUrl = process.env.NEXT_PUBLIC_API_URL || "https://calling.vercel.app";
+  
+  const baseUrl = process.env.NEXT_PUBLIC_API_URL || "https://duria-menu.vercel.app";
 
   try {
-   
+    
     const [resCat, resProd] = await Promise.all([
       fetch(`${baseUrl}/api/categories`, { cache: 'no-store' }),
-      fetch(`${baseUrl}/api/Products`, { cache: 'no-store' })
+      fetch(`${baseUrl}/api/products`, { cache: 'no-store' }) 
     ]);
 
-    if (!resCat.ok || !resProd.ok) throw new Error("API yanıt vermedi");
+    if (!resCat.ok || !resProd.ok) {
+      throw new Error("API yanıt vermedi");
+    }
 
     const categoryList = await resCat.json();
     const productList = await resProd.json();
